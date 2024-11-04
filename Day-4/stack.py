b6b97325375581ad13e3aed94a118cd14e93e226
Swadhin18 <144611471+Swadhin18@users.noreply.github.com>
2024-11-04 19:21:33 +0600

    These code are nothing just some samples, I guess tomorrow i will be able to learn NumPy and Pandas.

---
+++ Day-4/stack.py
@@ -0,0 +1,8 @@
+stacc = []
+stacc.append(3)
+stacc.append(5)
+stacc.append(9)
+stacc.append(2)
+print(stacc)
+stacc.pop()
+print(stacc[-1])
---
+++ Day-4/yoyo.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+for x in range(2, 30, 3):
+  print(x)
+  
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
